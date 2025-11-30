6ad4bfa4880262467e28d85421d646ddbe8c6b7d
sanathmayuram <ramakrishnasanathmayuram@gmail.com>
2025-11-30 11:41:13 +0530

    Hyper Parameter Tunning

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -40,24 +40,50 @@ class ModelTrainer:
             )
 
             models = {
-                "Random Forest" : RandomForestRegressor(),
-                "Decsion Tree" :
-                DecisionTreeRegressor(),
-                "Gradiant Boosting" :
-                GradientBoostingRegressor(),
-                "Liner Regression" :
-                LinearRegression(),
-                "K-Neighbors Classifier" :
-                KNeighborsRegressor(),
-                "XG Boost Classifier" :
-                XGBRegressor(),
-                "CatBoosting Classifier" :
-                CatBoostRegressor(verbose=False),
-                "AdaBoost Classifier" :
-                AdaBoostRegressor()
-            }
+    "Random Forest": RandomForestRegressor(),
+    "Decision Tree": DecisionTreeRegressor(),
+    "Gradient Boosting": GradientBoostingRegressor(),
+    "Linear Regression": LinearRegression(),
+    "KNN Regressor": KNeighborsRegressor(),
+    "XGB Regressor": XGBRegressor(),
+    "CatBoost Regressor": CatBoostRegressor(verbose=False),
+    "AdaBoost Regressor": AdaBoostRegressor()
+}
 
-            model_report:dict=evaluate_models(X_train=X_train,Y_train = Y_train,X_test = X_test, Y_test= Y_test , models=models)
+
+            params = {
+    "Decision Tree": {
+        'criterion': ['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+    },
+    "Random Forest": {
+        'n_estimators': [8,16,32,64,128,256]
+    },
+    "Gradient Boosting": {
+        'learning_rate': [.1,.01,.05,.001],
+        'subsample': [0.6, 0.7, 0.75, 0.8, 0.85, 0.9],
+        'n_estimators': [8,16,32,64,128,256]
+    },
+    "Linear Regression": {},
+    "KNN Regressor": {
+        'n_neighbors': [5,7,9,11]
+    },
+    "XGB Regressor": {
+        'learning_rate':[.1,.01,.05,.001],
+        'n_estimators':[8,16,32,64,128,256]
+    },
+    "CatBoost Regressor": {
+        'depth':[6,8,10],
+        'learning_rate':[0.01,0.05,.001],
+        'iterations':[30,50,100]
+    },
+    "AdaBoost Regressor": {
+        'learning_rate':[.1,.01,0.5,.001],
+        'n_estimators':[8,16,32,64,128,256]
+    }
+}
+
+
+            model_report:dict=evaluate_models(X_train=X_train,Y_train = Y_train,X_test = X_test, Y_test= Y_test , models=models,params=params)
 
             best_model_score = max(sorted(model_report.values()))
 
--- src/utils.py
+++ src/utils.py
@@ -6,7 +6,7 @@ import pandas as pd
 import dill
 
 from sklearn.metrics import r2_score
-
+from sklearn.model_selection import GridSearchCV
 from src.exception import CustonException
 
 def save_object(file_path, obj):
@@ -21,26 +21,37 @@ def save_object(file_path, obj):
     except Exception as e:
         raise CustonException(e, sys)
     
-def evaluate_models(X_train,Y_train,X_test, Y_test,models):
+def evaluate_models(X_train, Y_train, X_test, Y_test, models, params):
     try:
         report = {}
 
-        for i in range(len(list(models))):
-            model = list(models.values())[i]
+        # Loop using model names instead of values
+        for model_name, model in models.items():
 
+            # Get the parameter grid using the correct key
+            param_grid = params.get(model_name, {})
+
+            # Hyperparameter tuning
+            gs = GridSearchCV(model, param_grid, cv=3)
+            gs.fit(X_train, Y_train)
+
+            # Set best parameters
+            model.set_params(**gs.best_params_)
             model.fit(X_train, Y_train)
 
+            # Predictions
             y_train_pred = model.predict(X_train)
-
             y_test_pred = model.predict(X_test)
 
-            train_model_score = r2_score(Y_train, y_train_pred)
+            # Scores
+            train_score = r2_score(Y_train, y_train_pred)
+            test_score = r2_score(Y_test, y_test_pred)
 
-            test_model_score = r2_score(Y_test, y_test_pred)
-
-            report[list(models.keys())[i]] = test_model_score
+            # Save the final test score
+            report[model_name] = test_score
 
         return report
-    
+
     except Exception as e:
         raise CustonException(e, sys)
+
