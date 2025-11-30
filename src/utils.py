f804dfb9f3fb61bb92d21b94a55f227cc3eefa8e
sanathmayuram <ramakrishnasanathmayuram@gmail.com>
2025-11-30 12:50:44 +0530

    PipeLine

--- src/utils.py
+++ src/utils.py
@@ -54,4 +54,13 @@ def evaluate_models(X_train, Y_train, X_test, Y_test, models, params):
 
     except Exception as e:
         raise CustonException(e, sys)
+    
+
+def load_object(file_path):
+    try :
+        with open(file_path, "rb") as file_obj:
+            return dill.load(file_obj)
+        
+    except Exception as e:
+        raise CustonException(e, sys)
 
